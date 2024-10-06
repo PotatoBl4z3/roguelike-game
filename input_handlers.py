e465b452deba222d359584900a9b2d7386e116a3
Sreevatsa Surya Tanikella <tanikella.sreevatsa@gmail.com>
2024-10-06 00:58:44 -0500

    Add Deaths, Corpses and Stats

--- input_handlers.py
+++ input_handlers.py
@@ -4,16 +4,59 @@ from typing import Optional, TYPE_CHECKING #importing Python's type hinting syst
 
 import tcod.event #importing tcod's event system
 
-from actions import Action, EscapeAction, BumpAction #importing the Action class and its subclasses from actions.py
+from actions import Action, BumpAction, EscapeAction, WaitAction #importing the Action class and its subclasses from actions.py
 
 if TYPE_CHECKING:
     from engine import Engine
+    
+MOVE_KEYS = {
+    # Arrow keys.
+    tcod.event.K_UP: (0, -1),
+    tcod.event.K_DOWN: (0, 1),
+    tcod.event.K_LEFT: (-1, 0),
+    tcod.event.K_RIGHT: (1, 0),
+    tcod.event.K_HOME: (-1, -1),
+    tcod.event.K_END: (-1, 1),
+    tcod.event.K_PAGEUP: (1, -1),
+    tcod.event.K_PAGEDOWN: (1, 1),
+    # Numpad keys.
+    tcod.event.K_KP_1: (-1, 1),
+    tcod.event.K_KP_2: (0, 1),
+    tcod.event.K_KP_3: (1, 1),
+    tcod.event.K_KP_4: (-1, 0),
+    tcod.event.K_KP_6: (1, 0),
+    tcod.event.K_KP_7: (-1, -1),
+    tcod.event.K_KP_8: (0, -1),
+    tcod.event.K_KP_9: (1, -1),
+    # Vi keys.
+    tcod.event.K_h: (-1, 0),
+    tcod.event.K_j: (0, 1),
+    tcod.event.K_k: (0, -1),
+    tcod.event.K_l: (1, 0),
+    tcod.event.K_y: (-1, -1),
+    tcod.event.K_u: (1, -1),
+    tcod.event.K_b: (-1, 1),
+    tcod.event.K_n: (1, 1),
+}
+
+WAIT_KEYS = {
+    tcod.event.K_PERIOD,
+    tcod.event.K_KP_5,
+    tcod.event.K_CLEAR,
+}
 
 #Class that handles all Events
 class EventHandler(tcod.event.EventDispatch[Action]):
     def __init__(self, engine: Engine):
         self.engine = engine
+        
+    def handle_events(self) -> None:
+        raise NotImplementedError()
 
+    def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
+        raise SystemExit()
+    
+class MainGameEventHandler(EventHandler):
     def handle_events(self) -> None:
         for event in tcod.event.wait():
             action = self.dispatch(event)
@@ -26,8 +69,6 @@ class EventHandler(tcod.event.EventDispatch[Action]):
             self.engine.handle_enemy_turns()
             self.engine.update_fov()  # Update the FOV before the players next action.
             
-    def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]: #function to quit if event is Quit. This method is overriding EventDispatch's ev_quit function
-        raise SystemExit
     
     def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]: #function to check which key was pressed and return the respective action
         action: Optional[Action] = None #setting the action to None as default. This will be returned if no key press is found
@@ -36,17 +77,35 @@ class EventHandler(tcod.event.EventDispatch[Action]):
         
         player = self.engine.player
         
-        if key == tcod.event.K_UP:
-            action = BumpAction(player, dx = 0, dy = -1)
-        elif key == tcod.event.K_DOWN:
-            action = BumpAction(player, dx = 0, dy = 1)
-        elif key == tcod.event.K_RIGHT:
-            action = BumpAction(player, dx=1, dy=0)
-        elif key == tcod.event.K_LEFT:
-            action = BumpAction(player, dx=-1, dy=0)
+        if key in MOVE_KEYS:
+            dx, dy = MOVE_KEYS[key]
+            action = BumpAction(player, dx, dy)
+        elif key in WAIT_KEYS:
+            action = WaitAction(player)
             
         elif key == tcod.event.K_ESCAPE:
             action = EscapeAction(player)
         
         #No valid key was pressed
+        return action
+
+class GameOverEventHandler(EventHandler):
+    def handle_events(self) -> None:
+        for event in tcod.event.wait():
+            action = self.dispatch(event)
+
+            if action is None:
+                continue
+
+            action.perform()
+
+    def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
+        action: Optional[Action] = None
+
+        key = event.sym
+
+        if key == tcod.event.K_ESCAPE:
+            action = EscapeAction(self.engine.player)
+
+        # No valid key was pressed
         return action
